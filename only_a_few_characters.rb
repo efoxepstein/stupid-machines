4bdefbaf1ad525b7dc83be4f97b957c750008a31
Eli <eli@elilies.com>
2011-06-16 13:02:39 -0400

    Much shorter

--- only_a_few_characters.rb
+++ only_a_few_characters.rb
@@ -1,2 +1,2 @@
 # Cyclic Tag System starting with 11001 with rules 010, 000, 1111
-a,b,i,c='11001',%w[010 000 1111],0,->{(p a[i..-1])>'09'&&a+=b[i%3];a[i+=1]&&c[]};c[]
+a,b,i='11001',%w[010 000 1111],-1;(p a[i..-1])>'09'&&a+=b[i%3] while a[i+=1]
