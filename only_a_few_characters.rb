712631d708a151fef87a2397c6a73a807084e818
Eli <eli@elilies.com>
2011-06-16 13:16:59 -0400

    shortest yet

--- only_a_few_characters.rb
+++ only_a_few_characters.rb
@@ -1,2 +1,2 @@
 # Cyclic Tag System starting with 11001 with rules 010, 000, 1111
-a,b,i='11001',%w[010 000 1111],1;(p a)>'09'&&a=a[1..-1]+b[(i+=1)%3] while a[0]
+a='11001';%w[010 000 1111].cycle{|x|a=a[1..-1]+((p a)>'09'?x:'');a[0]||1/0}
