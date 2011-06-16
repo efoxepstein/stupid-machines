05b5ef32ac6b626be377682ee3acdb9be5ba19fe
Eli <eli@elilies.com>
2011-06-16 13:20:06 -0400

    Chop a character

--- only_a_few_characters.rb
+++ only_a_few_characters.rb
@@ -1,2 +1,2 @@
 # Cyclic Tag System starting with 11001 with rules 010, 000, 1111
-a='11001';%w[010 000 1111].cycle{|x|a=a[1..-1]+((p a)>'09'?x:'');a[0]||1/0}
+a='11001';%w[010 000 1111].cycle{|x|a=a[1..-1]+((p a)>'09'?x:'');1/a.size}
